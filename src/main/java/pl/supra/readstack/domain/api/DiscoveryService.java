c25d5db7077b6f888a3ad8fb952402dff26b0509
Mateusz Ludwin <mateusz.ludwin@gmail.com>
2021-11-23 14:02:44 +0100

    Vote buttons implementation

--- src/main/java/pl/supra/readstack/domain/api/DiscoveryService.java
+++ src/main/java/pl/supra/readstack/domain/api/DiscoveryService.java
@@ -12,45 +12,47 @@ public class DiscoveryService {
     private final DiscoveryDao discoveryDao = new DiscoveryDao();
     private final DiscoveryMapper discoveryMapper = new DiscoveryMapper();
 
-    public void add(DiscoverySaveRequest saveRequest){
-            Discovery discoveryToSave = discoveryMapper.map(saveRequest);
-            discoveryDao.save(discoveryToSave);
+    public void add(DiscoverySaveRequest saveRequest) {
+        Discovery discoveryToSave = discoveryMapper.map(saveRequest);
+        discoveryDao.save(discoveryToSave);
     }
 
-    public List<DiscoveryBasicInfo>findAll(){
+    public List<DiscoveryBasicInfo> findAll() {
         return discoveryDao.findAll()
                 .stream().map(discoveryMapper::map)
                 .collect(Collectors.toList());
     }
 
-    public List<DiscoveryBasicInfo> findByCategory(int categoryId){
-        return discoveryDao.findByCategory(categoryId).stream()
-                .map(discoveryMapper::map)
+    public List<DiscoveryBasicInfo> findAllByCategory(int categoryId) {
+        return discoveryDao.findByCategory(categoryId)
+                .stream().map(discoveryMapper::map)
                 .collect(Collectors.toList());
     }
 
     private static class DiscoveryMapper {
         private final UserDao userDao = new UserDao();
 
-         DiscoveryBasicInfo map(Discovery d){
+        DiscoveryBasicInfo map(Discovery d) {
             return new DiscoveryBasicInfo(
+                    d.getId(),
                     d.getTitle(),
                     d.getUrl(),
                     d.getDescription(),
                     d.getDateAdded()
             );
         }
-        Discovery map(DiscoverySaveRequest ds){
-             return new Discovery(
-                     ds.getTitle(),
-                     ds.getUrl(),
-                     ds.getDescription(),
-                     LocalDateTime.now(),
-                     ds.getCategoryId(),
-                     userDao.findByUsername(ds.getAuthor())
-                     .orElseThrow()
-                     .getId()
-             );
+
+        Discovery map(DiscoverySaveRequest ds) {
+            return new Discovery(
+                    ds.getTitle(),
+                    ds.getUrl(),
+                    ds.getDescription(),
+                    LocalDateTime.now(),
+                    ds.getCategoryId(),
+                    userDao.findByUsername(ds.getAuthor())
+                            .orElseThrow()
+                            .getId()
+            );
         }
     }
 }
